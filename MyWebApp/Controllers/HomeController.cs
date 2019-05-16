db504f2c29526ac46f7660342787358ac666a68b
Michael Ibukun Fabiyi <mikelfab@yahoo.com>
2019-05-16 20:11:36 +0100

    changed title

--- MyWebApp/Controllers/HomeController.cs
+++ MyWebApp/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MyWebApp.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page 1";
+            ViewBag.Title = "Home Page";
 
             return View();
         }
